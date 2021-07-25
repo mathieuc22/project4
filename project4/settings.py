f3b52b3e3d7fc42a95b7c260370d798d6fa40397
Mathieu <mathieu.collet@gmail.com>
2021-07-25 20:42:49 +0200

    add DEFAULT_AUTO_FIELD for models

--- project4/settings.py
+++ project4/settings.py
@@ -70,6 +70,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project4.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
