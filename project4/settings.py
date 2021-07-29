9d7447238590fc18a38d64e02039ee45881b7bcf
Mathieu <mathieu.collet@gmail.com>
2021-07-29 18:04:53 +0200

    launching to production

--- project4/settings.py
+++ project4/settings.py
@@ -128,13 +128,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Static files settings
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
